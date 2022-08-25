556cbba67c39b1f8d6f38506ad2f45fcf10a568f
anna <slukin1@yandex.ru>
2022-08-25 23:22:37 +0700

    tesk41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -38,13 +38,11 @@ int CountPosElem(int[] array)
 
 void PrintArray(int[] array)
 {
-    // Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
         if (i < array.Length - 1) Console.Write($"{array[i]},");
         else Console.Write(array[i]);
     }
-    // Console.WriteLine("]");
 }
 
 int[] arr = GetArray(size);
